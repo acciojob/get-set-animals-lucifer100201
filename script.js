b6cd96037a5ea43bfcdeb0f785af1046f5599e9d
lucifer100201 <99162740+lucifer100201@users.noreply.github.com>
2024-04-22 01:54:05 -0600

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 class Animal {
     constructor(species){
-        this._species = species; // Renamed to _species
+        this._species = species; 
     }
 
-    get species(){ // Renamed the getter method
-        return this._species; // Changed to return _species
+    get species(){ 
+        return this._species; 
     }
 
     makeSound(){
-        console.log(`The ${this.species} makes a sound.`);
+        console.log(`The ${this.species} makes a sound`);
     }
 }
 
